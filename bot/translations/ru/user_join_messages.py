4b0fd5595addba2ab74e46afdcedf40b71267c59
serhioth <senchos.ork@gmail.com>
2024-04-27 12:07:29 +0300

    fixed imports again

--- bot/translations/ru/user_join_messages.py
+++ bot/translations/ru/user_join_messages.py
@@ -1,10 +1,10 @@
-from bot.core.constants import ANSWER_TIMEOUT
+from bot.core.config import settings
 
 USER_JOIN_MESSAGE = (
     'Приветствуем тебя, {username}! '
     'Пожалуйста, докажи свою принадлежность к людям. '
-    'Нажми на кнопку с символом - "{symbol}."\n'
-    f'У тебя есть {ANSWER_TIMEOUT} секунд, чтобы подумать. '
+    'Нажми на кнопку с символом - "{symbol}".\n'
+    f'У тебя есть {settings.captcha_answer_timeout} секунд, чтобы подумать. '
     'Выбирай мудро!'
 )
 
